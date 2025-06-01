679ff1c15bb864802ee0248b4ad70617c2b60160
Suan Eden <silveirasuan@gmail.com>
2025-05-31 23:15:30 -0300

    Adiciona arquivo .bat e script para criação de usuário admin

---
+++ sportsnews/sportsnews/create_superuser.py
@@ -0,0 +1,19 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sportsnews.settings")
+django.setup()
+
+User = get_user_model()
+
+username = "admin"
+email = "silveirasuan@gmail.com"
+password = "admin"
+
+if not User.objects.filter(username=username).exists():
+    print("Criando superusuário...")
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superusuário criado com sucesso.")
+else:
+    print("Superusuário já existe.")
