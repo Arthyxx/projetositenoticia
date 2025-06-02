905f9155e8a624214b6404020cc0659c6230fbd7
Suan Eden <167621360+SuanEden@users.noreply.github.com>
2025-06-02 16:49:29 -0300

    Update create_superuser.py
    Corrigindo a falta do primeiro import do django

--- sportsnews/sportsnews/create_superuser.py
+++ sportsnews/sportsnews/create_superuser.py
@@ -1,19 +1,24 @@
+import sys
 import os
 import django
 from django.contrib.auth import get_user_model
 
+# Adiciona o diretório raiz ao sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# Define as configurações do Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sportsnews.settings")
 django.setup()
 
+# Criação do superusuário
 User = get_user_model()
 
 username = "admin"
-email = "silveirasuan@gmail.com"
 password = "admin"
 
 if not User.objects.filter(username=username).exists():
     print("Criando superusuário...")
-    User.objects.create_superuser(username=username, email=email, password=password)
+    User.objects.create_superuser(username=username, password=password)
     print("Superusuário criado com sucesso.")
 else:
     print("Superusuário já existe.")
